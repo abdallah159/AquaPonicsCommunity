01d96e125417cd554eb75b1cd112dc6479953c24
abdallah159 <abdallah.mohammed.fciscu@gmail.com>
2018-07-01 17:24:46 +0200

    edit notification

--- app/src/main/java/in/tvac/akshayejh/photoblog/NotificationRecyclerAdapter.java
+++ app/src/main/java/in/tvac/akshayejh/photoblog/NotificationRecyclerAdapter.java
@@ -1,14 +1,17 @@
 package in.tvac.akshayejh.photoblog;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,8 +39,24 @@ public class NotificationRecyclerAdapter extends RecyclerView.Adapter<Notificati
     public List<BlogPost> blog_list;
     public Context context;
 
+    String userName ;
+    String userImage ;
+
+    long millisecond ;
+    String dateString ;
+    String user_id ;
+
+
+    TextView userNameDialog , dateDialog , descDialog , commentsDialog , likesDialog;
+    CircleImageView userImageDialog ;
+
+    ImageView blogPostImageDialog ;
+
+
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth firebaseAuth;
+
+    Dialog myDialog ;
 
     public NotificationRecyclerAdapter(List<BlogPost> blog_list){
 
@@ -56,7 +75,7 @@ public class NotificationRecyclerAdapter extends RecyclerView.Adapter<Notificati
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
 
         holder.setIsRecyclable(false);
 
@@ -67,7 +86,7 @@ public class NotificationRecyclerAdapter extends RecyclerView.Adapter<Notificati
         String image_url = blog_list.get(position).getImage_url();
         String thumbUri = blog_list.get(position).getImage_thumb();
 
-        String user_id = blog_list.get(position).getUser_id();
+         user_id = blog_list.get(position).getUser_id();
         //User Data will be retrieved here...
         firebaseFirestore.collection("Users").document(user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -75,8 +94,8 @@ public class NotificationRecyclerAdapter extends RecyclerView.Adapter<Notificati
 
                 if(task.isSuccessful()){
 
-                    String userName = task.getResult().getString("name");
-                    String userImage = task.getResult().getString("image");
+                     userName = task.getResult().getString("name");
+                     userImage = task.getResult().getString("image");
 
                     holder.setUserData(userName, userImage);
 
@@ -90,15 +109,76 @@ public class NotificationRecyclerAdapter extends RecyclerView.Adapter<Notificati
             }
         });
 
+
+        myDialog = new Dialog(context);
+        myDialog.setContentView(R.layout.notification_content);
+
+
+
+
+
         try {
-            long millisecond = blog_list.get(position).getTimestamp().getTime();
-            String dateString = DateFormat.format("MM/dd/yyyy", new Date(millisecond)).toString();
+             millisecond = blog_list.get(position).getTimestamp().getTime();
+             dateString = DateFormat.format("MM/dd/yyyy", new Date(millisecond)).toString();
             holder.setTime(dateString);
         } catch (Exception e) {
 
             Toast.makeText(context, "Exception : " + e.getMessage(), Toast.LENGTH_SHORT).show();
 
         }
+
+
+
+        userNameDialog = myDialog.findViewById(R.id.blog_user_name);
+        dateDialog = myDialog.findViewById(R.id.date_and_time);
+        likesDialog = myDialog.findViewById(R.id.blog_like_count);
+        commentsDialog = myDialog.findViewById(R.id.blog_comment_count);
+        descDialog = myDialog.findViewById(R.id.blog_desc);
+
+        userImageDialog = myDialog.findViewById(R.id.blog_user_image);
+        blogPostImageDialog = myDialog.findViewById(R.id.blog_image);
+
+
+
+
+
+        holder.item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                firebaseFirestore.collection("Users").document(user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        if(task.isSuccessful()){
+
+                            userName = task.getResult().getString("name");
+                            userImage = task.getResult().getString("image");
+
+                            holder.setUserData(userName, userImage);
+
+
+                        } else {
+
+                            //Firebase Exception
+
+                        }
+
+                    }
+                });
+
+
+                RequestOptions placeholderOption = new RequestOptions();
+                placeholderOption.placeholder(R.drawable.profile_placeholder);
+
+                Glide.with(context).applyDefaultRequestOptions(placeholderOption).load(blog_list.get(position).getImage_url()).into(blogPostImageDialog);
+
+
+
+                myDialog.show();
+
+            }
+        });
 
 
 
@@ -114,17 +194,21 @@ public class NotificationRecyclerAdapter extends RecyclerView.Adapter<Notificati
 
         private View mView;
 
+
         private ImageView blogImageView;
         private TextView blogDate;
-
-
         private TextView blogUserName;
         private CircleImageView blogUserImage;
+
+
+        private ConstraintLayout item ;
 
 
         public ViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
+
+            item = mView.findViewById(R.id.notification_item_id);
 
 
         }
@@ -144,11 +228,13 @@ public class NotificationRecyclerAdapter extends RecyclerView.Adapter<Notificati
             blogUserName = mView.findViewById(R.id.blog_user_name);
 
             blogUserName.setText(name);
+            userNameDialog.setText(name);
 
             RequestOptions placeholderOption = new RequestOptions();
             placeholderOption.placeholder(R.drawable.profile_placeholder);
 
             Glide.with(context).applyDefaultRequestOptions(placeholderOption).load(image).into(blogUserImage);
+            Glide.with(context).applyDefaultRequestOptions(placeholderOption).load(image).into(userImageDialog);
 
         }
 
